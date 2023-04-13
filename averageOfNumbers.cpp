76937adcef4e8e59f224fe9ed20e2c5ee16572c2
hibatasneem <hibat920@gmail.com>
2023-04-13 23:07:03 +0500

    average of numbers

---
+++ averageOfNumbers.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+ using namespace std;
+ int main(){
+int num1=2;
+int num2=3;
+int num3=4;
+int total=num1+num2+num3;
+int average=total/3;
+cout<<"the value of average is "<<average<<endl;
+
+int num4;
+cout << "Enter the value of num4: ";
+cin >> num4;
+
+int num5;
+cout<<"enter the value of num5"<<endl;
+cin>> num5;
+
+int num6;
+cout<<"enter the value of num6 is "<<endl;
+cin>> num6;
+
+float sum=num4+num5+num6;
+
+int number;
+cout<<"which no you want to divide by :";
+cin>>number;
+float average1=sum/number;
+cout<<"the value of average is "<<average1<<endl;
+
+
+
+ }
